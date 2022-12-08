bd5750824d2cbcb22c70817722d9506b50335160
David Garrett <84642851+BlueWaterBottles@users.noreply.github.com>
2022-12-08 02:03:46 -0500

    Started Chapter 12 Lab 1

---
+++ Chapter12/src/chapter12/Lab1.java
@@ -0,0 +1,32 @@
+/*
+ * David Garrett
+ * Chapter 12 Lab 1
+ * 12-8-22
+ */
+
+package chapter12;
+
+import java.util.Scanner;
+
+public class Lab1 
+{
+	Scanner input = new Scanner(System.in);
+	String fileName;
+	char fileOperation;
+	
+	// try?
+	System.out.print("What is the name of the file you wish to work with? ");
+	fileName = input.next();
+	
+	// try?
+	System.out.print("Are you going to (r)ead the file or (w)rite to the file? ");
+	fileOperation = input.nextChar();
+	
+	if (fileOperation == 'r')
+	{
+		
+	}
+	
+	
+	
+}
